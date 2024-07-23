bb4fe2b004d160e146f72c41fac1afca9b646d90
ToddKraines <melissa.null@unitn.it>
2024-07-23 13:11:50 +0200

    Update Skriptmaybe.m

--- Skriptmaybe.m
+++ Skriptmaybe.m
@@ -1,27 +1,10 @@
-addpath ('C:\Users\melis\Documents\Trento\Verona_Experiment\YS_visGamma_20240716\eeg\')
 cfg = [];
 cfg.dataset = 'YS-VisGAMMA_def.eeg';
+cfg.trialfun                = 'ft_trialfun_general'; % this is the default
+cfg.trialdef.eventtype      = 'Stimulus';
+cfg.trialdef.eventvalue     = ['S 4']; % the values of the stimulus trigger for the three conditions
 
-cfg.trialdef.prestim        = 1;
-cfg.trialdef.poststim       = 2.8;
-cfg.trialdef.std_triggers   = 1;
-cfg.trialdef.stim_triggers  = ['S 4'];
-cfg.trialdef.odd_triggers   = 2;
-cfg.trialdef.rsp_triggers   = [256 4096];
-cfg.trialfun                = 'trialfun_oddball_stimlocked';
-cfg                         = ft_definetrial(cfg);
+cfg.trialdef.prestim        = 1; % in seconds
+cfg.trialdef.poststim       = 2.8; % in seconds
 
-cfg.continuous              = 'yes';
-cfg.hpfilter                = 'no';
-cfg.detrend                 = 'no';
-cfg.continuous              = 'yes';
-cfg.demean                  = 'yes';
-cfg.dftfilter               = 'yes';
-cfg.dftfreq                 = [50 100];
-cfg.channel                 = 'EEG';
-
-cfg.reref                   = 'yes'; % recorded with left mastoid
-cfg.refchannel              = 'all';
-
-data_EEG                    = ft_preprocessing(cfg);
-save data_EEG data_EEG -v7.3
+cfg = ft_definetrial(cfg);
