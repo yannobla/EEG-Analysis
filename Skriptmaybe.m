9595d79cc7adea54077d8a8a2dadaf1145e0d5df
ToddKraines <melissa.null@unitn.it>
2024-07-25 14:58:28 +0200

    Update Skriptmaybe.m

--- Skriptmaybe.m
+++ Skriptmaybe.m
@@ -1,13 +1,33 @@
+addpath ('C:\Users\melis\Documents\Trento\Verona_Experiment\YS_visGamma_20240716\eeg\')
 %% segment data into trials with correct trialfunction
-cfg                         = [];
-cfg.dataset                 = 'YS-VisGAMMA_def.vhdr';
-cfg.trialfun                = 'trialfun_visgam'; 
-trialdata = ft_definetrial(cfg);
-preproc_data = ft_preprocessing(trialdata);
-cfg.demean      = 'yes'
-cfg.detrend     = 'yes'
-rawdata_1 = ft_databrowser(cfg, preproc_data)
-rawdata_2 = ft_databrowser(cfg, rawdata_1) % incase you want to continue working on rawdata
+cfg                 = [];
+cfg.trialfun        = 'trialfun_visgam'; 
+cfg.headerfile      = 'YS-VisGAMMA_def.vhdr';
+cfg.datafile        = 'YS-VisGAMMA_def.eeg';
+trialdata           = ft_definetrial(cfg);
+%% preprocessing and rereferencing
+cfg.implicitref     = 'LM';
+cfg.reref           = 'yes';
+cfg.refchannel      = {'LM' 'RM'};
+preproc_data        = ft_preprocessing(trialdata);
+%% resampling
+cfg                 = [];
+cfg.resamplefs      = 300;
+resampdata          = ft_resampledata(cfg, preproc_data);
+%% data browser to mark artifacts
+cfg                 = [];
+cfg.demean          = 'yes';
+cfg.detrend         = 'yes';
+brodata             = ft_databrowser(cfg, resampdata);
+%brodata_2          = ft_databrowser(cfg, brodata) % incase you want to continue working on rawdata
+% remember the time of the artifacts
+cfg_artfctdef       = brodata.artfctdef;
+%% artifact rejection
+cfg                     = [];
+cfg.artifactdef         = cfg_artfctdef;
+cfg.artfctdef.reject    = 'nan';
+data_clean              = ft_rejectartifact(cfg, resampdata);
+%% FP1 and FP2 need to be removed
 %%
 % cfg = [];
 % cfg.method = 'summary';
@@ -24,11 +44,11 @@ rawdata_2 = ft_databrowser(cfg, rawdata_1) % incase you want to continue working
 cfg.demean          = 'yes';
 cfg.baselinewindow  = [-0.2 0];
 %% filtering
-cfg.dftfilter   = 'yes'
+cfg.dftfilter   = 'yes';
 % cfg.dataset   = 'YS-VisGAMMA_def.eeg';
 % cfg.hpfreq    = 0.5;
 % cfg.hpfilter  = 'yes';
-trialdata = ft_preprocessing(cfg);
+trialdata       = ft_preprocessing(cfg);
 
 
 
